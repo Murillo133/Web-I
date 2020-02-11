e3ff2ab12cd15c2c5d1661d3f5496d0a09fe201e
Murillo133 <erickmurillo133@gmail.com>
2020-02-10 23:56:48 -0600

    Second commite

--- page_1.php
+++ page_1.php
@@ -157,14 +157,17 @@
 
 
 <?php
-echo "<h2>Your Input:</h2>";
-echo $name;
+echo "<h2>Input:</h2>";
+echo "Your name is ".$name;
+echo "<br>";
+echo $id;
 echo "<br>";
 echo $email;
 echo "<br>";
 echo $comment;
 echo "<br>";
 echo $gender;
+
 
 ?>
 
